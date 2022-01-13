3347c3f352dfd831e26162f48a972678b552004b
Aleksandr Tikka <altikka@student.hive.fi>
2022-01-13 16:50:00 +0200

    simple main that opens and closes. skeleton fillit.c

--- sources/fillit.c
+++ sources/fillit.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fillit.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: altikka & ememde <@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/13 14:59:23 by altikka           #+#    #+#             */
-/*   Updated: 2022/01/13 16:48:26 by altikka          ###   ########.fr       */
+/*   Created: 2022/01/13 16:12:13 by altikka           #+#    #+#             */
+/*   Updated: 2022/01/13 16:48:27 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fillit.h>
 
-int	main(int argc, char **argv)
+int	fillit(const int fd)
 {
-	int	fd;
-
-	if (argc != 2)
-		return (-1);
-	 fd = open(argv[1], O_RDONLY);
-	 if (fd == -1)
-		 return (-1);
-	 fillit(fd);
-	 if (close(fd) == -1)
-		 return(-1);
 	return (0);
 }
