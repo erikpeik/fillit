3c568bbceb99f772ceb4498ff8f7f9cdacf8b584
Aleksandr Tikka <altikka@student.hive.fi>
2022-01-14 13:55:54 +0200

    Cleaned GNL's header

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,15 +6,13 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 14:02:05 by altikka           #+#    #+#             */
-/*   Updated: 2022/01/12 16:37:09 by altikka          ###   ########.fr       */
+/*   Updated: 2022/01/14 13:54:21 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
-
-# include "libft.h"
 
 # define BUFF_SIZE	5
 # define FD_SIZE	8192
