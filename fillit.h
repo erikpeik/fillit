ced5b66c0b7ecc1ea172bf4ed092ac97dd9951c1
Erik Mende <emende@student.hive.fi>
2022-01-20 12:57:33 +0200

    Added prototype for block_append function

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 13:50:52 by altikka           #+#    #+#             */
-/*   Updated: 2022/01/19 23:31:37 by emende           ###   ########.fr       */
+/*   Updated: 2022/01/20 12:54:49 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ typedef struct s_block
 int		fillit(const int fd);
 int		*validate(const int fd);
 t_block	*block_new(const int *pos);
+void	block_append(t_block **head, const int *pos);
 void	print_map(char **map);
 char	**free_map(char **map);
 
