5086a339cd647832c8dfa075a6c2a679dd666721
Erik Mende <erikpeik@outlook.com>
2022-01-20 22:28:54 +0200

    Added error messages to main.c

--- main.c
+++ main.c
@@ -6,24 +6,30 @@
 /*   By: altikka & emende <@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 14:59:23 by altikka           #+#    #+#             */
-/*   Updated: 2022/01/20 21:50:33 by emende           ###   ########.fr       */
+/*   Updated: 2022/01/20 22:14:06 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "libft.h"
 
+static int	error_handle(void)
+{
+	ft_putendl("error");
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	int	fd;
 
-	if (argc != 2)
+	if (argc != 2 && error_handle())
 		return (-1);
 	fd = open(argv[1], O_RDWR);
-	if (fd == -1)
+	if (fd == -1 && error_handle())
 		return (-1);
 	fillit(fd);
-	if (close(fd) == -1)
+	if (close(fd) == -1 && error_handle())
 		return (-1);
 	return (0);
 }
