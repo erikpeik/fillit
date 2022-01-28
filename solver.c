15d8b15ba0e672f5c4768d1db3d545e61b8f4b3b
Aleksandr Tikka <altikka@student.hive.fi>
2022-01-28 16:16:59 +0200

    Removed unnesassery ret line after removing tetrimino. Cleaned comments out. Still too long.

--- solver.c
+++ solver.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   solver.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emende <emende@student.42.fr>              +#+  +:+       +#+        */
+/*   By: altikka & emende <@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 15:17:19 by altikka           #+#    #+#             */
-/*   Updated: 2022/01/27 15:56:06 by altikka          ###   ########.fr       */
+/*   Updated: 2022/01/28 16:15:54 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,21 +46,11 @@ int	solver(t_block *tet, int ***map, size_t *size)
 	{
 		if (ret == 1)
 		{
-//			ft_putstr("placing:  ");
-//			ft_putchar((char )(tet->n + 64));
-//			ft_putchar('\n');
 			place(tet, map, tet->n);
-//			print_result(*map, *size);
 			if (solver(tet->next, map, size) == 1)
 				return (1);
 			else
-			{
-//				ft_putstr("removing: ");
-//				ft_putchar((char )(tet->n + 64));
-//				ft_putchar('\n');
 				place(tet, map, 0);
-				ret = does_it_fit(tet, *map, *size);
-			}
 		}
 		if (ret != -120)
 			move_right(tet->pos);
@@ -77,39 +67,3 @@ int	solver(t_block *tet, int ***map, size_t *size)
 	}	
 	return (0);
 }
-
-/*int	solver(t_block *tet, int ***map, size_t *size)
-{
-	int	ret;
-
-	if (tet == NULL)
-		return (1);
-	ret = does_it_fit(tet, *map, *size);
-	while (ret != -121)
-	{
-		if (ret == 0)
-			move_right(tet->pos);
-		if (ret == -120)
-			move_left_and_down(tet->pos);
-		if (ret == 1)
-		{
-			place(tet, map, tet->n);
-			if (solver(tet->next, map, size) == 1)
-				return (1);
-			else
-			{
-				place(tet, map, 0);
-				move_right(tet->pos);
-			}
-		}
-		ret = does_it_fit(tet, *map, *size);
-	}
-	move_top_left(tet->pos);
-	if (ret == -121 && tet->n == 1)
-	{
-		*map = recreate_map(*map, *size + 1);
-		*size += 1;
-		solver(tet, map, size);
-	}	
-	return (0);
-}*/
