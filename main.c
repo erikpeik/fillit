edebc901afdc574ecc4953d09d28546e060cf5d7
Aleksandr Tikka <altikka@student.hive.fi>
2022-02-03 14:00:22 +0200

    changed error_handle to use err_exit fun

--- main.c
+++ main.c
@@ -6,18 +6,12 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 14:59:23 by altikka           #+#    #+#             */
-/*   Updated: 2022/02/02 17:26:12 by emende           ###   ########.fr       */
+/*   Updated: 2022/02/03 13:58:31 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "libft.h"
-
-static int	error_handle(void)
-{
-	ft_putendl("error");
-	return (1);
-}
 
 int	main(int argc, char **argv)
 {
@@ -25,15 +19,15 @@ int	main(int argc, char **argv)
 	int		**map;
 	size_t	size;
 
-	if (argc != 2 && error_handle())
+	if (argc != 2 && err_exit())
 		return (-1);
 	fd = open(argv[1], O_RDWR);
-	if (fd == -1 && error_handle())
+	if (fd == -1 && err_exit())
 		return (-1);
 	map = NULL;
 	size = 0;
 	fillit(fd, &map, size);
-	if (close(fd) == -1 && error_handle())
+	if (close(fd) == -1 && err_exit())
 		return (-1);
 	return (0);
 }
