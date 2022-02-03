e830c7e74fadbbcd871ab06a3a4c1f9530d98c44
Aleksandr Tikka <altikka@student.hive.fi>
2022-02-03 14:49:17 +0200

    Removed leaks tests before all possible exits.

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 16:12:13 by altikka           #+#    #+#             */
-/*   Updated: 2022/02/02 17:34:34 by emende           ###   ########.fr       */
+/*   Updated: 2022/02/03 14:43:40 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,21 @@ static void	get_blocks(const int fd, t_block **head)
 	pos = NULL;
 	ret = lost_and_found(fd, &pos);
 	if (ret == -1 && err_exit())
-	{
-		system("leaks fillit");
 		exit(1);
-	}
 	*head = block_new((const int *) pos);
 	if (*head == NULL && err_pos(pos))
-	{
-		system("leaks fillit");
 		exit(1);
-	}
 	ft_memdel((void **) &pos);
 	count = 1;
 	while (ret != 0)
 	{
 		if (count >= 26 && err_blocks(*head))
-		{
-			system("leaks fillit");
 			exit(1);
-		}
 		ret = lost_and_found(fd, &pos);
 		if (ret == -1 && err_all(pos, *head))
-		{
-			system("leaks fillit");
 			exit(1);
-		}
 		if (block_append(head, (const int *) pos) == -1 && err_all(pos, *head))
-		{
-			system("leaks fillit");
 			exit(1);
-		}
 		ft_memdel((void **) &pos);
 		count++;
 	}
@@ -66,10 +51,7 @@ int	fillit(const int fd, int ***map, size_t size)
 	size = map_min_size(head);
 	(*map) = create_map(size);
 	if (*map == NULL && err_blocks(head))
-	{
-		system("leaks fillit");
 		exit(1);
-	}
 	solver(head, map, &size, 0);
 	print_result(*map, size);
 	free_map((void **) *map, size);
